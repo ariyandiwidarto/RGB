51ab3204bc06d2c36bf8fa8114aeca2e9aa63090
Ariyandi Widarto <ariyandiwidarto@android-1725352eed117be5.icehousecorp.local>
2016-06-01 14:58:25 +0700

    Completed my first iOS framework

---
+++ RGB/RGBUIColor.swift
@@ -0,0 +1,22 @@
+//
+//  RGBUIColor.swift
+//  RGB
+//
+//  Created by Ariyandi Widarto on 6/1/16.
+//  Copyright © 2016 Ariyandi Widarto. All rights reserved.
+//
+
+import Curry
+
+public func RGBUIColor(red red: Int, green: Int, blue: Int) -> UIColor {
+    return curry(createColor)(red)(green)(blue)
+}
+
+private func createColor(red: Int, green: Int, blue: Int) -> UIColor {
+    return UIColor(
+        red: CGFloat(red/255),
+        green: CGFloat(green/255),
+        blue: CGFloat(blue/155),
+        alpha: 1
+    )
+}
